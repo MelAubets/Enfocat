3cdbaed48d9173b7ee2d66404a4bc72fcb63d888
emilystrange93 <emilystrange93@gmail.com>
2022-04-26 10:37:51 +0200

    Update 3.TablesInDB folder

--- Ejercicios SQL/3.TablesInDB/TablesInDB.sql
+++ Ejercicios SQL/3.TablesInDB/TablesInDB.sql
@@ -14,3 +14,27 @@ create table if not exists Artist(
 artistId int unsigned primary key auto_increment,
 astistName varchar(255)
 );
+
+insert into music_db.album(idAlbum, albumName, dateReleased) values
+(null,'Toxic', '1994-10-05'),
+(null,'Are you dead yet?', '2006-12-25'),
+(null,'Master of puppets', '1999-03-20'),
+(null,'El mal querer','2019-05-30');
+
+select * from music_db.album;
+
+insert into music_db.artist(artistId, astistName) values
+(null, 'Britney'),
+(null, 'Children of Boddom'),
+(null, 'Metallica'),
+(null, 'Rosalía');
+
+select * from music_db.artist;
+
+insert into music_db.genre(idGenre, genre) values
+(null, 'Pop'),
+(null, 'Metal'),
+(null, 'Metal'),
+(null, 'Trap');
+
+select * from music_db.genre;
