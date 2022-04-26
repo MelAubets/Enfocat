c9989ab5266774fd27d9be71b11c92bf542e674c
emilystrange93 <emilystrange93@gmail.com>
2022-04-26 11:28:16 +0200

    Office_db exercise

---
+++ Ejercicios SQL/4.Selects/office_selector.sql
@@ -0,0 +1,9 @@
+use office_db;
+
+select * from office_db.departments;
+select first_name, last_name, phone_number from office_db.employees;
+select first_name as nombre, last_name as apellido, phone_number as telefono from office_db.employees;
+select * from employees where phone_number='515.127.4565';
+delete from employees where phone_number='515.127.4565';
+select * from employees where phone_number='515.127.4565';
+ 
