08466c21738d23905c9e54901a7099030a6adf19
emilystrange93 <emilystrange93@gmail.com>
2022-05-04 10:09:36 +0200

    java rainbow!

---
+++ Java/Rainbow/src/Rainbow.java
@@ -0,0 +1,15 @@
+
+public class Rainbow {
+
+	public static void main(String[] args) {
+		System.out.println("\033[48;2;148;0;211m                         \033[0m");
+		System.out.println("\033[48;2;75;0;130m                         \033[0m");
+		System.out.println("\033[48;2;0;0;255m                         \033[0m");
+		System.out.println("\033[48;2;0;255;0m                         \033[0m");
+		System.out.println("\033[48;2;255;255;0m                         \033[0m");
+		System.out.println("\033[48;2;255;127;211m                         \033[0m");
+		System.out.println("\033[48;2;255;0;0m                         \033[0m");
+		
+	}
+
+}
